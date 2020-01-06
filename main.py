590346167560373c3a9e509f4e8726dd8ed50556
Jean-Sebastien BEVILACQUA <realitix@gmail.com>
2020-01-06 18:18:01 +0100

    Add version

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import json
 from os import path
+import time;
 
 from fdb.datasources import Datasources
 
@@ -10,8 +11,16 @@ HERE = path.dirname(path.abspath(__file__))
 def main():
     datasources = Datasources()
     data = datasources.generate()
+    version = int(time.time())
+    out = {
+        "version": version,
+        "aliments": data
+    }
     with open(path.join(HERE, 'out', 'data.json'), 'w') as outfile:
-        json.dump(data, outfile)
+        json.dump(out, outfile)
+
+    with open(path.join(HERE, 'out', 'version.json'), 'w') as outfile:
+        json.dump({"version": version}, outfile)
 
 
 if __name__ == '__main__':
